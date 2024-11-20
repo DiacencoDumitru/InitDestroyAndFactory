f63b3c9eefed618ce17fcbc96e9a52ecfbe32a0b
Dumitru <119630796+DiacencoDumitru@users.noreply.github.com>
2024-11-20 16:29:26 +0200

    Dependency Injection and Readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-* Мы вручную внедряли зависимость с помощью Java кода. (следующий урок сделаем с помощью Spring)
+* Теперь не внедряем зависимости вручную как мы это делали до этого, а это мы доверили Spring'у с помощью `<constructor-arg ref="musicBean"/>` (Dependency Injection)
+* Изначально когда Spring читает файл `applicationContext.xml` он создаёт бины начиная у нас от `musicBean` и затем он уже дошёл до нашего бина `musicPlayer` и внедрил этот бин musicBean таким образом совершив Dependency Injection
--- src/main/java/practice/springcourse/TestSpring.java
+++ src/main/java/practice/springcourse/TestSpring.java
@@ -3,20 +3,16 @@ package practice.springcourse;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-/*
- 1. Сделаем внедрение зависимостей вручную, в дальнейшем мы будем делать это с помощью Spring Framework
- 2. Здесь мы получили зависимость из Spring Context'a и внедрили его в конструктор нашего MusicPlayer где передали music
- 3. после того как мы поработали с ApplicationContext мы обязательно должны его закрыть
- */
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+        // Music music = context.getBean("musicBean", Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
 
